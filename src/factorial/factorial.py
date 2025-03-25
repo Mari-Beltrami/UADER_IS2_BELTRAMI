53da8d2f295901e5ee78899b375afaff200fb371
Mari-Beltrami <MARIALOURDESBELTRAMI@GMAIL.COM>
2025-03-24 21:20:59 -0300

    Agregar ingreso de rango

--- src/factorial/factorial.py
+++ src/factorial/factorial.py
@@ -1,18 +1,18 @@
 #!/usr/bin/python
 #*-------------------------------------------------------------------------*
 #* factorial.py                                                            *
-#* calcula el factorial de un número                                       *
+#* calcula el factorial de un número o rango                               *
 #* Dr.P.E.Colla (c) 2022                                                   *
 #* Creative commons                                                        *
 #*-------------------------------------------------------------------------*
 import sys
+
 def factorial(num): 
     if num < 0: 
         print("Factorial de un número negativo no existe")
         return 0
     elif num == 0: 
         return 1
-        
     else: 
         fact = 1
         while(num > 1): 
@@ -20,12 +20,22 @@ def factorial(num):
             num -= 1
         return fact 
 
-
+#si se omite el número como argumento, solicitarlo
 if len(sys.argv) < 2:
-    #si se omite el número como argumento solicitarlo.
-    num = int(input("Ingrese un número para calcular su factorial: "))
+    entrada = input("Ingrese un número o rango (ej. 5 o 4-8): ")
 else:
-    num = int(sys.argv[1])
+    entrada = sys.argv[1]  
 
-print("Factorial", num, "! es", factorial(num))
+#saber si es núm único o rango
+if '-' in entrada:
+    partes = entrada.split('-')
+    desde = int(partes[0])
+    hasta = int(partes[1])
+    print(f"Calculando factoriales entre {desde} y {hasta}")
+    for i in range(desde, hasta + 1):
+        print(f"{i}! = {factorial(i)}")
+else:
+    num = int(entrada)
+    print(f"{num}! = {factorial(num)}")
+
 
